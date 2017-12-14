90469e9b0f9d73d14990740c0f3284d0386f6b53
Takuya Sato <sasuke.tak@gmail.com>
2017-12-14 20:35:03 +0900

    起動の高速化のため、リストアするバッファを 10 に制限した

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -4,7 +4,8 @@
 
 ;; save a list of open files in ~/.emacs.d/.emacs.desktop
 (setq desktop-path (list user-emacs-directory)
-      desktop-auto-save-timeout 600)
+      desktop-auto-save-timeout 600
+      desktop-restore-eager 10)
 (desktop-save-mode 1)
 
 (defun sanityinc/desktop-time-restore (orig &rest args)
