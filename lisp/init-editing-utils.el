f61dea69beb7d510cb54d7d008aabbf14eb95c9e
Takuya Sato <sasuke.tak@gmail.com>
2018-05-31 18:56:47 +0900

    マージミスでゴミコードが入っていたのを修正

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -248,12 +248,11 @@
 ;;----------------------------------------------------------------------------
 ;; Cut/copy the current line if no region is active
 ;;----------------------------------------------------------------------------
-<<<<<<< HEAD
 ;; (require-package 'whole-line-or-region)
 ;; (add-hook 'after-init-hook 'whole-line-or-region-mode)
 ;; (after-load 'whole-line-or-region
-;;   (diminish 'whole-line-or-region-mode))
-
+;;   (diminish 'whole-line-or-region-local-mode))
+;; 
 ;; (defun suspend-mode-during-cua-rect-selection (mode-name)
 ;;   "Add an advice to suspend `MODE-NAME' while selecting a CUA rectangle."
 ;;   (let ((flagvar (intern (format "%s-was-active-before-cua-rectangle" mode-name)))
@@ -271,30 +270,6 @@
 ;;              (,mode-name 1)))))))
 
 ;; (suspend-mode-during-cua-rect-selection 'whole-line-or-region-mode)
-=======
-(require-package 'whole-line-or-region)
-(add-hook 'after-init-hook 'whole-line-or-region-mode)
-(after-load 'whole-line-or-region
-  (diminish 'whole-line-or-region-local-mode))
-
-(defun suspend-mode-during-cua-rect-selection (mode-name)
-  "Add an advice to suspend `MODE-NAME' while selecting a CUA rectangle."
-  (let ((flagvar (intern (format "%s-was-active-before-cua-rectangle" mode-name)))
-        (advice-name (intern (format "suspend-%s" mode-name))))
-    (eval-after-load 'cua-rect
-      `(progn
-         (defvar ,flagvar nil)
-         (make-variable-buffer-local ',flagvar)
-         (defadvice cua--activate-rectangle (after ,advice-name activate)
-           (setq ,flagvar (and (boundp ',mode-name) ,mode-name))
-           (when ,flagvar
-             (,mode-name 0)))
-         (defadvice cua--deactivate-rectangle (after ,advice-name activate)
-           (when ,flagvar
-             (,mode-name 1)))))))
-
-(suspend-mode-during-cua-rect-selection 'whole-line-or-region-mode)
->>>>>>> upstream/master
 
 
 
