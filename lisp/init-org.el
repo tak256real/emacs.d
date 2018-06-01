13cf500a846def18eb47eaf8ca390cc8492eb785
Takuya Sato <sasuke.tak@gmail.com>
2018-06-01 17:09:25 +0900

    OrgMode の日時表記を英語に変更

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -16,7 +16,8 @@
       org-fast-tag-selection-single-key 'expert
       org-html-validation-link nil
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 80)
+      org-tags-column 80
+      system-time-locale "C")
 
 
 ;; Lots of stuff from http://doc.norang.ca/org-mode.html
