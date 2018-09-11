0fc8d2c5d34a84c7c08b18fc16c18dcd183404f9
Takuya Sato <sasuke.tak@gmail.com>
2018-09-11 14:21:31 +0900

    refs #108 markdown-mode の SKK 変換における確定の RET 押下の問題 暫定対応

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,5 +3,12 @@
   (after-load 'whitespace-cleanup-mode
     (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)))
 
+(defun my--markdown-entery-key-ad (this-func &rest args)
+  "markdown-modeでskk-henkan-mode中にエンターすると行頭にカーソルが飛んでしまう問題の対応"
+  (if skk-henkan-mode (skk-kakutei)
+    (apply this-func args)))
+
+(advice-add #'markdown-enter-key :around #'my--markdown-entery-key-ad)
 
 (provide 'init-markdown)
+
