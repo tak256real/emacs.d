e646098663f41e9de627a118c6e40f3dccc0f7e2
Takuya Sato <satoh-ta@riso.co.jp>
2019-02-06 10:53:37 +0900

    migemo chenge to cmigemo

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -7,6 +7,15 @@
       desktop-auto-save-timeout 600
       desktop-restore-eager 10)
 (desktop-save-mode 1)
+(setq desktop-buffers-not-to-save
+      (concat "\\("
+              "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+              "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb"
+              "\\)$"))
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+(add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
 
 (defun sanityinc/desktop-time-restore (orig &rest args)
   (let ((start-time (current-time)))
