c61dcc4d9c8ab48de3fa8d652620bdc59e85ccf7
Takuya Sato <sasuke.tak@gmail.com>
2020-06-05 17:38:56 +0900

    update

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,15 +4,30 @@
 
 ;; フォント
 ;; 半角英字設定
-(set-face-attribute 'default nil :family "Cica" :height 90)
+(set-face-attribute 'default nil :family "Cica" :height 120)
+
+;; ASCII 以外の Unicode.(ASCII はデフォルトで設定する)
+(set-fontset-font nil
+                  '(#x80 . #x10ffff)
+                  (font-spec :family "Cica"))
+
 ;; 全角かな設定
 (set-fontset-font (frame-parameter nil 'font)
                   'japanese-jisx0208
-                  (font-spec :family "Cica" :size 12))
+                  (cons "Cica" "iso10646-1"))
+
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (cons "Cica" "iso10646-1"))
+
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0212
+                  (cons "Cica" "iso10646-1"))
 ;; 半角ｶﾅ設定
 (set-fontset-font (frame-parameter nil 'font)
                   'katakana-jisx0201
-                  (font-spec :family "Cica" :size 12))
+                  (cons "Cica" "iso10646-1"))
+
 ;;;; ずれ確認用
 ;;;; 0123456789012345678901234567890123456789
 ;;;; ｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵ
@@ -49,10 +64,12 @@
 ;; (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)) ; *.h ファイルもcpp-modeで
 
 (defun my-c-mode-common-hook ()
-  (setq comment-start "// ")            ; コメントの書き出しを // にする
-  (setq comment-end "")                 ; コメントの書き終わり
-  (hide-ifdef-mode 1)                  ; hide-ifdef-mode をフックする
-  (helm-gtags-mode 1)                   ; gtags-mode をフックする
+  (setq truncate-lines t)                 ; 行は折り返さない.
+  (setq truncate-partial-width-windows t) ; バッファ幅が狭かろうと行は折り返さない.
+  (setq comment-start "// ")          ; コメントの書き出しを // にする
+  (setq comment-end "")               ; コメントの書き終わり
+  (hide-ifdef-mode 1)                 ; hide-ifdef-mode をフックする
+  (helm-gtags-mode 1)                 ; gtags-mode をフックする
   (auto-revert-mode 1) ; 他のエディタなどがファイルを書き換えたらすぐにそれを反映
   )
 
--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -10,6 +10,9 @@
  recentf-max-saved-items 5120
  recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
 
+; run-with-idle-timer 30 t '(lambda ()          ;; 30秒ごとに .recentf を保存
+;                             (with-suppressed-message (recentf-save-list))))
+
 
 (provide 'init-recentf)
 ;;; init-recentf.el ends here
